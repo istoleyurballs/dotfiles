9daff4f801633687179464d34cfd43be65efe5fa
Lucas Malandrino <lucas.malandrino@gmail.com>
2026-05-21 11:59:17 +0200

    fix(hyprland): forgot to migrate the lock screen shortcut

--- hyprland/dot-config/hypr/hyprland-base.lua
+++ hyprland/dot-config/hypr/hyprland-base.lua
@@ -262,6 +262,7 @@ hl.bind(mainMod .. " + SHIFT + E",
 -- hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + L", hl.dsp.exec_cmd("uwsm app -- hyprlock"))
 -- hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 -- hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit")) -- dwindle only
 
