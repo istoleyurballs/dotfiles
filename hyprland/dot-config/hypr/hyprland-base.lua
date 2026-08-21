e85f029535c29f62d45fdf59f3cd5892229501ce
Lucas Malandrino <lucas.malandrino@gmail.com>
2026-08-21 16:32:24 +0200

    feat(hyprland,noctalia): updated noctalia to latest

--- hyprland/dot-config/hypr/hyprland-base.lua
+++ hyprland/dot-config/hypr/hyprland-base.lua
@@ -41,7 +41,7 @@ TERMINAL          = "alacritty"
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function()
-    hl.exec_cmd("qs -c noctalia-shell")
+    hl.exec_cmd("noctalia")
     hl.exec_cmd("systemctl --user start hyprsunset.service")
     hl.exec_cmd("systemctl --user start hyprpolkitagent.service")
 end)
@@ -252,7 +252,7 @@ hl.gesture({
 ---------------------
 
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
-local noctalia = "qs -c noctalia-shell ipc call" -- Prefix for noctalia keybinds
+local noctalia = "noctalia msg" -- Prefix for noctalia keybinds
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
 hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(TERMINAL))
@@ -261,8 +261,9 @@ hl.bind(mainMod .. " + SHIFT + E",
     hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 -- hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(noctalia .. " launcher toggle"))
-hl.bind(mainMod .. " + L", hl.dsp.exec_cmd(noctalia .. " lockScreen lock"))
+hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(noctalia .. " panel-toggle launcher"))
+hl.bind(mainMod .. " + L", hl.dsp.exec_cmd(noctalia .. " session lock"))
+hl.bind("Print", hl.dsp.exec_cmd(noctalia .. " screenshot-region"))
 -- hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 -- hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit")) -- dwindle only
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen())
@@ -305,17 +306,17 @@ hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Laptop multimedia keys for volume and LCD brightness
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd(noctalia .. " volume increase"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd(noctalia .. " volume decrease"), { locked = true, repeating = true })
-hl.bind("XF86AudioMute", hl.dsp.exec_cmd(noctalia .. " volume muteOutput"), { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd(noctalia .. " volume micInput"), { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd(noctalia .. " volume-up"), { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd(noctalia .. " volume-down"), { locked = true, repeating = true })
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd(noctalia .. " volume-mute"), { locked = true, repeating = true })
+hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd(noctalia .. " mic-mute"), { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
 -- Requires playerctl
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd(noctalia .. " media next"), { locked = true })
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd(noctalia .. " media playPause"), { locked = true })
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(noctalia .. " media playPause"), { locked = true })
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd(noctalia .. " media toggle"), { locked = true })
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(noctalia .. " media toggle"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(noctalia .. " media previous"), { locked = true })
 
 -- Cycle keyboard layouts
@@ -363,7 +364,10 @@ hl.window_rule({
 -- overlayLayerRule:set_enabled(false)
 hl.layer_rule({
     name = "noctalia",
-    match = { namespace = "noctalia-background-.*$" },
+    match = {
+        namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+    },
+    no_anim = true,
     ignore_alpha = 0.5,
     blur = true,
     blur_popups = true,
